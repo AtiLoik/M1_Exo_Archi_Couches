996958fde20d756872fd1b857f9d220c3c31cf98
Loik FR <32639716+AtiLoik@users.noreply.github.com>
2020-02-13 15:58:12 +0100

    Documentation du Main

--- src/com/ensup/data/presentation/Main.java
+++ src/com/ensup/data/presentation/Main.java
@@ -11,22 +11,28 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		/*BufferReader = Permet de lire les informations entrées dans la console;
+		Créer un étudiant pour interagir avec la BDD*/
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		EtudiantService service = new EtudiantService();
 		
 		try {
 			System.out.println("Bienvenue dans le programme Etudiant");
 			String choix;
+			/*Affiche les interactions dans la console, jusqu'a ce qu'un choix valide soit fait*/
 			do {
 				System.out.println("Quelle action souhaitez vous faire ?");
-				System.out.println("1 - Cr�er un utilisateur");
-				System.out.println("2 - R�cuperer informations");
+				System.out.println("1 - Créer un utilisateur");
+				System.out.println("2 - Récuperer informations");
 				
 				System.out.print("");
 				
 				choix = br.readLine();
 			}while(!choix.equals("1") && !choix.equals("2"));
 			
+			/*Si un choix valide est effectué:
+			- 1: Insere une personne dans la BDD;
+			- 2: Affiche les détails d'une personne*/
 			if(choix.equals("1")) {
 				System.out.print("Quel est votre prenom: ");
 				String prenom = br.readLine();
@@ -43,16 +49,16 @@ public class Main {
 				
 				service.createEtudiant(prenom, nom, email);
 			}else if(choix.equals("2")) {
-				System.out.print("Quel id souhaitez vous recup�rer ?");
+				System.out.print("Quel id souhaitez vous recupérer ?");
 				String id = br.readLine();
 				Etudiant etudiant = service.getEtudiantById(id);
 				if(etudiant != null) {
 					System.out.println(etudiant.toString());
 				}else {
-					System.out.println("Aucun utilisateur ne correspond � cet ID !");
+					System.out.println("Aucun utilisateur ne correspond à cet ID !");
 				}
 			}
-			
+		/*Affiche une exception en cas de problemes*/	
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
