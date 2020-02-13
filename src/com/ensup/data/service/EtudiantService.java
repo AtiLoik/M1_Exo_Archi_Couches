24dbea88804b57f61958a1ce9808c037f4fe54f3
Loik FR <32639716+AtiLoik@users.noreply.github.com>
2020-02-13 15:51:19 +0100

    Documentation de EtudiantServices

--- src/com/ensup/data/service/EtudiantService.java
+++ src/com/ensup/data/service/EtudiantService.java
@@ -5,16 +5,20 @@ import com.ensup.data.domaine.Etudiant;
 
 public class EtudiantService {
 
+	/*Attributs: */
 	private EtudiantDAO etudiantDao;
 	
+	/*Constructeur: */
 	public EtudiantService() {
 		etudiantDao = new EtudiantDAO();
 	}
 	
+	/*Permet de créer un étudiant*/
 	public void createEtudiant(String prenom, String nom, String email) {
 		etudiantDao.createEtudiant(prenom, nom, email);
 	}
 	
+	/*Permet de récupérer les information d'un étudiant (via le toString)*/
 	public Etudiant getEtudiantById(String id) {
 		return etudiantDao.getEtudiantById(id);
 	}
